17e45cab81bc7a3ff5e8f594e7e5d05f8288b635
levipaulk <levipaulk@gmail.com>
2019-05-30 10:49:27 -0400

    testing deployment connection

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
+    PORT: process.env.PORT || 8080,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/benrinote',
     TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/benrinote',
-  }
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api',
+};
